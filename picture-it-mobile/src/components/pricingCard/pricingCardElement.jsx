f830cc2634b391704c3b97bd28a5fd9eaac97cb4
Dylan Guzman <>
2022-11-14 16:01:38 -0800

    - Update File

---
+++ picture-it-mobile/src/components/pricingCard/pricingCardElement.jsx
@@ -0,0 +1,12 @@
+import './pricingCard.css';
+
+function PricingCardElement({title, type}) {
+    return ( 
+        <div className='benefit-element-container'>
+            <i style = {{color: 'rgb(42, 182, 145', fontSize: '15px'}} class="fa-regular fa-circle-check"></i>
+            <p style={{color: type == 'dark' ? 'white' : 'black'}} className='pricing-card-element-text'> {title} </p>
+        </div>
+     );
+}
+
+export default PricingCardElement;
