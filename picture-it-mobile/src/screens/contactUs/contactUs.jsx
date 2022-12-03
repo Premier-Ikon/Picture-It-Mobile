2ef838fd2acff29824046b5f5346322db718ea65
Dylan Guzman <>
2022-12-02 18:01:37 -0800

    - Recent changes need to start styling for mobiel

--- picture-it-mobile/src/screens/contactUs/contactUs.jsx
+++ picture-it-mobile/src/screens/contactUs/contactUs.jsx
@@ -1,11 +1,166 @@
-import React from 'react';
+import './contactUs.css';
+import {useState, useRef} from "react";
+
+import HeaderImage from '../../assets/contact-us-image.png';
 
 function ContactUs() {
+    const [data, setData] = useState({
+        name: "",
+        email: "",
+        phone: "",
+        address: "",
+        date: "",
+        message: "",
+    });
+
+    const form = useRef();
+
+    const {name, email, phone, address, date, message} = data
+
+    const handleChange = e => {
+        setData({...data, [e.target.name]: e.target.value});
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        alert("Thank you for your inquiry, someone on our team will be in contact as soon as possible . \n\n" +
+            "Please allow 24 business hours for a response. If your inquiry is urgent please call us at (951)458-3966")
+    }
+
     return ( 
         <div>
-            <div>
-                
+            <div className='contact-header-container'>
+                <div className='header-data-container'>
+                    <div className='form-data-container'>
+                        <div style={{marginBottom: '20px'}}>
+                            <h3 className='general-standard-header'>Get In Touch With Us</h3>
+                            <p style={{fontSize: '18px', marginTop: '-30px'}} className='about-us-general-text'>Fill out the form and an expert will contact you shortly</p>
+                        </div>
+                        <div className="form-data">
+                            <form ref={form} onSubmit={handleSubmit}>
+                                <div className="top-data">
+                                    <div>
+                                        <div className="data-header">
+                                            <label>Your name</label>
+                                        </div>
+                                        <input
+                                            type="text"
+                                            name={"name"}
+                                            autoComplete={"off"}
+                                            value={name}
+                                            onChange={handleChange}
+                                            className="data-box"
+                                        />
+                                    </div>
+                                    <div style={{marginLeft: '30px'}}>
+                                        <div className="data-header">
+                                            <label>Your email</label>
+                                        </div>
+                                        <input
+                                            type="email"
+                                            name={"email"}
+                                            autoComplete={"off"}
+                                            value={email}
+                                            onChange={handleChange}
+                                            className="data-box"
+
+                                        />
+                                    </div>
+                                </div>
+                                <div className="middle-data">
+                                    <div>
+                                        <div className="data-header">
+                                            <label>Your phone number</label>
+                                        </div>
+                                        <input
+                                            type="text"
+                                            name={"phone"}
+                                            autoComplete={"off"}
+                                            value={phone}
+                                            onChange={handleChange}
+                                            className="data-box"
+                                        />
+                                    </div>
+                                    <div style={{marginLeft: '30px'}}>
+                                        <div className="data-header">
+                                            <label>Your address</label>
+                                        </div>
+                                        <input
+                                            type="text"
+                                            name={"address"}
+                                            autoComplete={"off"}
+                                            value={address}
+                                            onChange={handleChange}
+                                            className="data-box"
+                                        />
+                                    </div>
+                                </div>
+                                <div>
+                                <div className='bottom-data'>
+                                    <div>
+                                        <div className="data-header">
+                                            <label>Date of event</label>
+                                        </div>
+                                        <input
+                                            type="text"
+                                            name={"date"}
+                                            autoComplete={"off"}
+                                            value={date}
+                                            onChange={handleChange}
+                                            className="data-box"
+                                        />
+
+                                    </div>
+                                </div>
+                                </div>
+                                <div style={{marginTop: '10px'}}>
+                                    <div className="data-header">
+                                        <label>Your message</label>
+                                    </div>
+                                    <textarea
+                                        name={"message"}
+                                        // rows={3}
+                                        autoComplete={"off"}
+                                        value={message}
+                                        className={"message-box"}
+                                        onChange={handleChange}
+                                    />
+                                </div>
+                                <div className={"submit-button-container"}>
+                                    <input type={"submit"} style={{cursor: "pointer"}} value={"Submit"} className="submit-button"/>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                    <div>
+                        <img style={{height: '470px', marginLeft: '50%', marginTop: '40px'}} src={HeaderImage} alt="Camera"/>
+                    </div>
+                </div>
             </div>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+                <div className='break'/>
+            </div>
+            <div>
+                <div>
+                    
+                </div>
+                <div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+
+                    </div>
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+
         </div>
      );
 }
